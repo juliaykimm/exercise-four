9703266dd238d0ba95124511d627455310b0cad7
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-11-05 14:40:14 -0500

    added sxpress and env files

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
-
+const express = require("express"); // "npm install express" to install express
 const app = express();
-
 const port = process.env.POST || 4000;
 
+// Routes Import
 const indexRoute = require("./routes/index.js");
+// Routes
+app.use("/", indexRoute);
 
 app.listen(port, () =>
-  console.log("Exercise Four is running at local host:${port}")
+  console.log(`Exercise Four is running at local host:${port}`)
 );
