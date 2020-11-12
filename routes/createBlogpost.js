3181834855649db70d65c065d9d163c0c409a188
juliaykimm <54916475+juliaykimm@users.noreply.github.com>
2020-11-12 14:51:29 -0500

    added button in createBlog

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -1,8 +1,22 @@
 // Creat a Blogpost
 const express = require("express");
-
 const router = express.Router();
+// Require Firebase
+const firebase = require("firebase");
+// Initialize Firebase Database
+const db = firebase.firestore();
+// Reference a specific collection
+const blogposts = db.collection("blogposts");
 
-router.get("/", (req, res) => res.send("Create a Blogpost"));
+const form = `
+<form>
+    <input type="text" name="title" placeholder="Title of Post"/>
+    <input type="text" name="text" placeholder="Text of Post"/>
+    <input type="text" name="author" placeholder="Author"/>
+    <button type="submit">Submit Post</button>
+</form>
+`;
+
+router.get("/", (req, res) => res.send(form));
 
 module.exports = router;
